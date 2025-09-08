726786a3e3db3021a889f3682f2e7aa66c1185a5
Dingleberry <edonist.boil854@passinbox.com>
2025-09-08 15:54:13 +0200

    Imroved comments and layout.

--- dymeman.h
+++ dymeman.h
@@ -5,13 +5,37 @@ see if any memory leak is happening, whenever the pointer you're allocating is N
 this library will NOT track it and will NOT consider it in the final report.
 Planning onto changing this in the future. */
 
+
+
+/* FLAGS SWITCHING */
+
+/* Switches the flag to the provided state.
+Default: thread_safe = true. */
+void thread_safety(bool state);
+
+/* Switches the flag to false.
+If called, no statistics will be saved nor anything will be printed to console.
+Default: final_report_flag = true */
+void final_report();
+
+
+
+/* CUSTOM ALLOCATORS/DEALLOCATORS - SAFE MEMORY MANAGEMENT FUNCTIONS */
+
 /* There is NO need to check for fails manually. Everything is done internally.
 Realloc returns the realloc-ed pointer, if successful, else the provided one.
 All pointers are freed autmatically upon program termination. */
 
+/* Meant for internal use. Refer to dmalloc(). */
 void* s_malloc(unsigned int size, const char *file, unsigned int line);
+
+/* Meant for internal use. Refer to dcalloc(). */
 void* s_calloc(unsigned int n, unsigned int size, const char *file, unsigned int line);
+
+/* Meant for internal use. Refer to drealloc(). */
 void* s_realloc(void *ptr, unsigned int newSize, const char *file, unsigned int line);
+
+/* Meant for internal use. Refer to dfree(). */
 void s_free(void *p, const char *file, unsigned int line);
 
 /* Safely mallocs, adding the ptr to the manager array */
@@ -26,25 +50,13 @@ void s_free(void *p, const char *file, unsigned int line);
 /* Safely frees, NULL-ifying the reference in the manager array */
 #define dfree(ptr) s_free(ptr, __FILE__, __LINE__)
 
-/* Switches the flag to the provided state.
-Default: thread_safe = true. */
-void thread_safety(bool state);
 
-/* Switches the flag to true.
-If called, the moment the first memory management operation is called, a new benchmark will start, lasting up to the end of the program.
-Default: auto_benchmark = false. */
-void auto_benchmark();
 
-/* Switches the flag to false.
-If called, no statistics will be saved nor anything will be printed to console.
-Default: final_report_flag = true */
-void final_report();
+/* BENCHMARKING FUNCTIONS */
 
 /* benchmark_create() creates a benchmark, with a provided unique tag.
-Benchmarking functions DO NOT return anything. They are tracked internally.
-Results of the benchmarks are tracked and displayed only when final_report_flag = true.
+Results of the benchmarks are tracked and displayed only when final_report_flag = true [for this, check final_report()].
 NOTE: tag uniqueness is NOT enforced. By using the same tags you may lose control over a benchmark. */
-
 void s_benchmark_create(const char *tag, const char *file, unsigned int line);
 void s_benchmark_stop(const char *tag, const char *file, unsigned int line);
 void s_benchmark_stop_all(const char *file, unsigned int line);
@@ -57,3 +69,24 @@ void s_benchmark_stop_all(const char *file, unsigned int line);
 
 /* Stops every benchmark. */
 #define benchmark_stop_all() s_benchmark_stop_all(__FILE__, __LINE__)
+
+
+
+/* MEMORY DIAGNOSIS - DEFAULT ALLOCATORS/DEALLOCATORS USED; NO MEMORY SAFETY GRANTED. */
+
+/* Ment for internal use. Refer to malloc(). */
+void* debug_malloc(unsigned int size, const char *file, unsigned int line);
+
+/* Meant for internal use. Refer to calloc(). */
+void* debug_calloc(unsigned int n, unsigned int size, const char *file, unsigned int line);
+
+/* Meant for internal use. Refer to realloc(). */
+void* debug_realloc(void *ptr, unsigned int size, const char *file, unsigned int line);
+
+/* Meant for internal use. Refer to free(). */
+void debug_free(void *ptr, const char *file, unsigned int line);
+
+#define malloc(size) debug_malloc(size, __FILE__, __LINE__)
+#define calloc(n, size) debug_calloc(n, size, __FILE__, __LINE__)
+#define realloc(ptr, size) debug_realloc(ptr, size, __FILE__, __LINE__)
+#define free(ptr) debug_free(ptr, __FILE__, __LINE__)
